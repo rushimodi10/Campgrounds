6d86a5febbddf88f29cb2f860be72d4e89b961f4
Rushi Modi <rushi.modi9@gmail.com>
2024-02-05 19:41:59 +0530

    Added some basic styling.

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
+          location: `${cities[random1000].city}, ${cities[random1000].state}`,
+          title: `${sample(descriptors)} ${sample(places)}`,
+          image: "https://source.unsplash.com/collection/483251",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis magnam numquam temporibus asperiores eum deserunt! Doloremque, autem maiores. Quae fugit sequi neque quibusdam aspernatur nesciunt enim nulla obcaecati, voluptate quos!",
+          price
+        });
         await camp.save();
     }
 }
